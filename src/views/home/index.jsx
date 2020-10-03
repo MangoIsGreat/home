17b94238384cfbd8190686409198c331247ea8d8
zp <859247268@qq.com>
2020-10-03 17:31:32 +0800

    feat: 轮播图完成

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -1,21 +1,64 @@
-import React, {Component} from "react"
+import React, { Component } from "react";
+import { Carousel } from "antd-mobile";
+import { BASE_URL } from "../../utils/url";
 
-import styles from "./index.module.scss"
+import styles from "./index.module.scss";
 
 export default class Home extends Component {
-    componentDidMount() {
-        this.getSwipperData()
-    }
+  constructor() {
+    super();
 
-    getSwipperData = () => {
-        this.axios.get('/home/swiper').then(res => {
-            console.log(res)
-        })
-    }
+    this.state = {
+      swipers: null,
+    };
+  }
 
-    render() {
-        return(
-            <div>Home</div>
-        )
+  componentDidMount() {
+    this.getSwipperData();
+  }
+
+  getSwipperData = async () => {
+    const result = await this.axios.get("/home/swiper");
+
+    if (result.data.status === 200) {
+      this.setState({
+        swipers: result.data.body,
+      });
     }
+  };
+
+  renderSwiper = () => {
+    return (
+      <Carousel autoplay={true} infinite>
+        {this.state.swipers.map((item) => (
+          <a
+            key={item.id}
+            href="http://www.alipay.com"
+            style={{ display: "inline-block", width: "100%", height: 212 }}
+          >
+            <img
+              src={`${BASE_URL}${item.imgSrc}`}
+              alt=""
+              style={{ width: "100%", verticalAlign: "top" }}
+              onLoad={() => {
+                // fire window resize event to change height
+                window.dispatchEvent(new Event("resize"));
+                this.setState({ imgHeight: "auto" });
+              }}
+            />
+          </a>
+        ))}
+      </Carousel>
+    );
+  };
+
+  render() {
+    const { swipers } = this.state;
+    return (
+      <div className={styles.root}>
+        {/* 渲染轮播图 */}
+        {swipers && this.renderSwiper()}
+      </div>
+    );
+  }
 }
