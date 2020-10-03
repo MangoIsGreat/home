e7db1008e1fb4876f50371d58e84e1e2ab2cd8fc
zp <859247268@qq.com>
2020-10-03 20:57:17 +0800

    feat: 完成首页导航菜单栏

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
-import { Carousel } from "antd-mobile";
+import { Carousel, Flex } from "antd-mobile";
 import { BASE_URL } from "../../utils/url";
+import { Link } from "react-router-dom";
 
 import styles from "./index.module.scss";
+
+import image1 from "../../assets/images/nav-1.png";
+import image2 from "../../assets/images/nav-2.png";
+import image3 from "../../assets/images/nav-3.png";
+import image4 from "../../assets/images/nav-4.png";
 
 export default class Home extends Component {
   constructor() {
@@ -12,6 +18,14 @@ export default class Home extends Component {
       swipers: null,
     };
   }
+
+  //   定义的实例属性
+  navs = [
+    { icon: image1, text: "整租", path: "/layout/houselist" },
+    { icon: image2, text: "合租", path: "/layout/houselist" },
+    { icon: image3, text: "地图找房", path: "/map" },
+    { icon: image4, text: "去出租", path: "/rent/add" },
+  ];
 
   componentDidMount() {
     this.getSwipperData();
@@ -52,13 +66,32 @@ export default class Home extends Component {
     );
   };
 
+  renderNavs() {
+    return (
+      <Flex className={styles.nav}>
+        {this.navs.map((item) => {
+          return (
+            <Flex.Item>
+              <Link to={item.path}>
+                <img src={item.icon} alt="" />
+                <p>{item.text}</p>
+              </Link>
+            </Flex.Item>
+          );
+        })}
+      </Flex>
+    );
+  }
+
   render() {
     const { swipers } = this.state;
-    
+
     return (
       <div className={styles.root}>
         {/* 渲染轮播图 */}
         {swipers && this.renderSwiper()}
+        {/* 首页导航菜单渲染 */}
+        {this.renderNavs()}
       </div>
     );
   }
