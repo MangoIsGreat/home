e0344d1adb4db47db5668b78e08907fab29a04c3
zp <859247268@qq.com>
2020-10-04 05:09:32 +0800

    feat: 城市选择的数据处理

--- src/views/cityList/index.jsx
+++ src/views/cityList/index.jsx
@@ -2,6 +2,35 @@ import React, { Component } from "react";
 import MyNavBar from "../../components/MyNavBar";
 
 export default class CityList extends Component {
+  componentDidMount() {
+    this.getCityData()
+  }
+
+  getCityData = async () => {
+    const result = await this.axios.get("/area/city?level=1")
+
+    this.dealWithCityData(result.data.body)
+  }
+
+  dealWithCityData = (cityData) => {
+    const tempObj = {}
+
+    cityData.forEach(city => {
+      const letter = city.short.substr(0, 1)
+
+      if (tempObj[letter]) {
+        tempObj[letter].push(city)
+      } else {
+        tempObj[letter] = [city]
+      }
+    });
+
+    const IndexObjectKeys = Object.keys(tempObj).sort()
+
+    console.log(tempObj)
+    console.log(IndexObjectKeys)
+  }
+
   render() {
     return (
       <div>
