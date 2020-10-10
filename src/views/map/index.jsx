112ded12ef3a5a03296c1d16272a36636a505c26
zp <859247268@qq.com>
2020-10-10 14:53:28 +0800

    feat: 完成了一级覆盖物的渲染

--- src/views/map/index.jsx
+++ src/views/map/index.jsx
@@ -5,9 +5,21 @@ import { getCurrentCity } from "../../utils/city.js";
 
 const BMap = window.BMap;
 
+// 圆形覆盖物的样式
+const labelStyle = {
+  cursor: "pointer",
+  border: "0px solid rgb(255, 0, 0)",
+  padding: "0px",
+  whiteSpace: "nowrap",
+  fontSize: "12px",
+  color: "rgb(255, 255, 255)",
+  textAlign: "center",
+};
+
 export default class Map extends Component {
   async componentDidMount() {
     const city = await getCurrentCity();
+    this.id = city.value;
 
     this.initMap(city.label);
   }
@@ -23,12 +35,45 @@ export default class Map extends Component {
       (point) => {
         if (point) {
           this.map.centerAndZoom(point, 11);
+
+          // 添加覆盖物
+          this.addOverlays(this.id);
         } else {
           alert("您选择地址没有解析到结果!");
         }
       },
       cityName
     );
+  };
+
+  // 添加覆盖物
+  addOverlays = async (id) => {
+    const result = await this.axios.get(`/area/map?id=${id}`);
+
+    result.data.body.forEach((item) => {
+      const {
+        label: name,
+        value,
+        count,
+        coord: { latitude, longitude },
+      } = item;
+
+      var point = new BMap.Point(longitude, latitude);
+
+      var opts = {
+        position: point, // 指定文本标注所在的地理位置
+        offset: new BMap.Size(30, -30), //设置文本偏移量
+      };
+      var label = new BMap.Label("", opts); // 创建文本标注对象
+      label.setStyle(labelStyle);
+      label.setContent(`
+      <div class=${styles.bubble}>
+        <p class=${styles.name}>${name}</p>
+        <p class=${styles.name}>${count}</p>
+      </div>
+    `);
+      this.map.addOverlay(label);
+    });
   };
 
   render() {
