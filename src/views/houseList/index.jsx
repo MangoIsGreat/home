f543eb7b9b806f8f1f2b010fe61c3dd8e3421950
zp <859247268@qq.com>
2020-10-12 23:22:22 +0800

    feat: 格式化代码

--- src/views/houseList/index.jsx
+++ src/views/houseList/index.jsx
@@ -1,34 +1,37 @@
-import React, {Component} from "react"
-import { Flex } from "antd-mobile"
-import SearchBar from "../../components/SearchBar"
-import { getCurrentCity } from "../../utils/city"
+import React, { Component } from "react";
+import { Flex } from "antd-mobile";
+import SearchBar from "../../components/SearchBar";
+import { getCurrentCity } from "../../utils/city";
 
-import styles from "./index.module.scss"
+import styles from "./index.module.scss";
 
 export default class houseList extends Component {
-    constructor() {
-        super()
+  constructor() {
+    super();
 
-        this.state = {
-            cityName: "深圳"
-        }
-    }
+    this.state = {
+      cityName: "深圳",
+    };
+  }
 
-    async componentDidMount() {
-        const result = await getCurrentCity()
-        this.setState({
-            cityName: result.label
-        })
-    }
+  async componentDidMount() {
+    const result = await getCurrentCity();
+    this.setState({
+      cityName: result.label,
+    });
+  }
 
-    render() {
-        return(
-            <div className={styles.root}>
-                <Flex className={styles.listHeader}>
-                    <i className="iconfont icon-back" />
-                    <SearchBar cityName={this.state.cityName} className={styles.mySearchBar} />
-                </Flex>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className={styles.root}>
+        <Flex className={styles.listHeader}>
+          <i className="iconfont icon-back" />
+          <SearchBar
+            cityName={this.state.cityName}
+            className={styles.mySearchBar}
+          />
+        </Flex>
+      </div>
+    );
+  }
 }
