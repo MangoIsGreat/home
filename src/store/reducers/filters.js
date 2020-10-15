fb128b76ad24f561d0d73544a33d23b7cd218d87
zp <859247268@qq.com>
2020-10-15 17:28:33 +0800

    feat: 完成了FilterTitle的高亮状态渲染

--- src/store/reducers/filters.js
+++ src/store/reducers/filters.js
@@ -28,10 +28,27 @@ export default (state = initState, action) => {
       const newState1 = JSON.parse(JSON.stringify(state));
       newState1.filterData = action.payload;
       return newState1;
+
     case SET_OPEN_TYPE:
       const newState2 = JSON.parse(JSON.stringify(state));
       newState2.openType = action.payload;
+
+      // 处理高亮状态
+      Object.keys(newState2.selectTitleValue).forEach((type) => {
+        if (type === "area") {
+          newState2.selectTitleValue["area"] =
+            newState2.selectValue["area"].length > 2;
+        } else if (type === "mode" || type === "price") {
+          newState2.selectTitleValue[type] =
+            newState2.selectValue[type][0] !== "null";
+        } else if (type === "more") {
+          newState2.selectTitleValue["more"] =
+            newState2.selectValue["more"].length > 0;
+        }
+      });
+
       return newState2;
+
     case SET_SELECT_TITLE_VALUE:
       const newState3 = JSON.parse(JSON.stringify(state));
       newState3.selectTitleValue = {
@@ -39,10 +56,12 @@ export default (state = initState, action) => {
         ...action.payload,
       };
       return newState3;
+
     case SET_VALUE:
       const newState4 = JSON.parse(JSON.stringify(state));
       newState4.selectValue = { ...newState4.selectValue, ...action.payload };
       return newState4;
+
     default:
       return state;
   }
