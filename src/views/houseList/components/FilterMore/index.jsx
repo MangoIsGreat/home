0736d2dd4a7ae3684743c4b57a83c78b9766e5a5
zp <859247268@qq.com>
2020-10-15 17:48:52 +0800

    feat: 渲染FilterMore组件

--- src/views/houseList/components/FilterMore/index.jsx
+++ src/views/houseList/components/FilterMore/index.jsx
@@ -1,11 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import styles from "./index.module.scss";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import * as filterActionCreator from "../../../../store/actionCreators/filterActionCreator";
 
-export default class FilterMore extends Component {
-    render() {
-        return (
-            <div>
-                FilterMore
-            </div>
-        )
-    }
+class FilterMore extends Component {
+  render() {
+    return (
+      <div className={styles.root}>
+        {/* 遮罩 */}
+        <div className={styles.mask}></div>
+        {/* 内容区域 */}
+        <div className={styles.tags}></div>
+      </div>
+    );
+  }
 }
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators(filterActionCreator, dispatch);
+};
+
+export default connect(null, mapDispatchToProps)(FilterMore);
