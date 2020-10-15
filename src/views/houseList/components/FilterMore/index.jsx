3e38389532e442200a3bdd42beb548436565e12c
zp <859247268@qq.com>
2020-10-15 20:16:23 +0800

    feat: 渲染FilterMore组件

--- src/views/houseList/components/FilterMore/index.jsx
+++ src/views/houseList/components/FilterMore/index.jsx
@@ -3,22 +3,72 @@ import styles from "./index.module.scss";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import * as filterActionCreator from "../../../../store/actionCreators/filterActionCreator";
+import FilterFooter from "../FilterFooter";
 
 class FilterMore extends Component {
+  renderItems = (data) => {
+    return (
+      <dd className={styles.dd}>
+        {data.map((item) => {
+          return (
+            <span className={styles.tag} key={item.value}>
+              {item.label}
+            </span>
+          );
+        })}
+      </dd>
+    );
+  };
+
   render() {
+    const { roomType, oriented, floor, characteristic } = this.props;
     return (
       <div className={styles.root}>
         {/* 遮罩 */}
-        <div className={styles.mask}></div>
+        <div
+          onClick={() => this.props.setOpenType("")}
+          className={styles.mask}
+        ></div>
         {/* 内容区域 */}
-        <div className={styles.tags}></div>
+        <div className={styles.tags}>
+          <dl className={styles.dl}>
+            <dt className={styles.dt}>户型</dt>
+            {this.renderItems(roomType)}
+            <dt className={styles.dt}>朝向</dt>
+            {this.renderItems(oriented)}
+            <dt className={styles.dt}>楼层</dt>
+            {this.renderItems(floor)}
+            <dt className={styles.dt}>房屋亮点</dt>
+            {this.renderItems(characteristic)}
+          </dl>
+        </div>
+        <div className={styles.footer}>
+          <FilterFooter
+            cancelText="清楚"
+            okClick={() => console.log(11111)}
+            cancelClick={() => console.log(22222)}
+          />
+        </div>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({
+  filters: {
+    filterData: { roomType, oriented, floor, characteristic },
+  },
+}) => {
+  return {
+    roomType,
+    oriented,
+    floor,
+    characteristic,
+  };
+};
+
 const mapDispatchToProps = (dispatch) => {
   return bindActionCreators(filterActionCreator, dispatch);
 };
 
-export default connect(null, mapDispatchToProps)(FilterMore);
+export default connect(mapStateToProps, mapDispatchToProps)(FilterMore);
