7d0527d5c0253837c206e714974b31ec86d07b07
zp <859247268@qq.com>
2020-10-19 01:23:45 +0800

    feat: 使用formik改造登录页面

--- src/views/login/index.jsx
+++ src/views/login/index.jsx
@@ -2,74 +2,39 @@ import React, { Component } from "react";
 import MyNavBar from "../../components/MyNavBar";
 import { WhiteSpace, WingBlank, Flex, Toast } from "antd-mobile";
 import { setToken } from "../../utils/token";
+import { Field, Form, withFormik } from "formik";
+import { axios } from "../../utils/axios";
 
 import styles from "./index.module.scss";
 
-export default class Login extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      username: "test2",
-      password: "test2",
-    };
-  }
-
-  changeValue = (e) => {
-    this.setState({
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  login = async (e) => {
-    e.preventDefault();
-
-    const result = await this.axios.post("/user/login", this.state);
-
-    if (result.data.status === 200) {
-      //   保存token
-      setToken(result.data.body.token);
-
-      // 跳转，返回
-      this.props.history.goBack();
-    } else {
-      Toast.info(result.data.description);
-    }
-  };
-
+class Login extends Component {
   render() {
-    const { username, password } = this.state;
-
     return (
       <div className={styles.root}>
         <MyNavBar title="账号登录" />
         <WhiteSpace size="lg" />
         <WingBlank>
-          <form onSubmit={this.login}>
+          <Form>
             <div className={styles.formItem}>
-              <input
-                className={styles.input}
-                value={username}
+              <Field
                 name="username"
+                className={styles.input}
                 placeholder="请输入账号"
                 type="text"
-                onChange={this.changeValue}
               />
             </div>
             <div className={styles.formItem}>
-              <input
-                className={styles.input}
-                value={password}
+              <Field
                 name="password"
+                className={styles.input}
                 placeholder="请输入密码"
                 type="text"
-                onChange={this.changeValue}
               />
             </div>
             <div className={styles.formSubmit}>
               <input className={styles.submit} value="登录" type="submit" />
             </div>
-          </form>
+          </Form>
           <Flex className={styles.backHome}>
             <Flex.Item>
               <a href="#/">还没有账号，去注册~</a>
@@ -80,3 +45,20 @@ export default class Login extends Component {
     );
   }
 }
+
+export default withFormik({
+  mapPropsToValues: () => ({ username: "", password: "" }),
+  handleSubmit: async (values, { props }) => {
+    const result = await axios.post("/user/login", values);
+
+    if (result.data.status === 200) {
+      // 保存token
+      setToken(result.data.body.token);
+
+      // 跳转，返回
+      props.history.goBack();
+    } else {
+      Toast.info(result.data.description, 1.5);
+    }
+  },
+})(Login);
