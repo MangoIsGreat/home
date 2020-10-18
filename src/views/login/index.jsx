e4e0b5e7c6aa46af17f0fcea1108c2882ac6bdd8
zp <859247268@qq.com>
2020-10-18 18:57:56 +0800

    feat: 完成了登录页面的渲染

--- src/views/login/index.jsx
+++ src/views/login/index.jsx
@@ -1,11 +1,37 @@
-import React, {Component} from "react"
+import React, { Component } from "react";
+import MyNavBar from "../../components/MyNavBar";
+import { WhiteSpace, WingBlank } from "antd-mobile";
 
-import styles from "./index.module.scss"
+import styles from "./index.module.scss";
 
 export default class Login extends Component {
-    render() {
-        return(
-            <div>Login</div>
-        )
-    }
+  render() {
+    return (
+      <div className={styles.root}>
+        <MyNavBar title="账号登录" />
+        <WhiteSpace size="lg" />
+        <WingBlank>
+          <form action="">
+            <div className={styles.formItem}>
+              <input
+                className={styles.input}
+                placeholder="请输入账号"
+                type="text"
+              />
+            </div>
+            <div className={styles.formItem}>
+              <input
+                className={styles.input}
+                placeholder="请输入密码"
+                type="text"
+              />
+            </div>
+            <div className={styles.formSubmit}>
+              <input className={styles.submit} value="登录" type="submit" />
+            </div>
+          </form>
+        </WingBlank>
+      </div>
+    );
+  }
 }
