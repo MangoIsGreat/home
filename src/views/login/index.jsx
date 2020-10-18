63a01bc9e80063945d07e5839f3a35d39ca3eca2
zp <859247268@qq.com>
2020-10-18 23:53:09 +0800

    feat: 完成了登录操作

--- src/views/login/index.jsx
+++ src/views/login/index.jsx
@@ -1,29 +1,69 @@
 import React, { Component } from "react";
 import MyNavBar from "../../components/MyNavBar";
-import { WhiteSpace, WingBlank, Flex } from "antd-mobile";
+import { WhiteSpace, WingBlank, Flex, Toast } from "antd-mobile";
+import { setToken } from "../../utils/token";
 
 import styles from "./index.module.scss";
 
 export default class Login extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      username: "test2",
+      password: "test2",
+    };
+  }
+
+  changeValue = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  login = async (e) => {
+    e.preventDefault();
+
+    const result = await this.axios.post("/user/login", this.state);
+
+    if (result.data.status === 200) {
+      //   保存token
+      setToken(result.data.body.token);
+
+      // 跳转，返回
+      this.props.history.goBack();
+    } else {
+      Toast.info(result.data.description);
+    }
+  };
+
   render() {
+    const { username, password } = this.state;
+
     return (
       <div className={styles.root}>
         <MyNavBar title="账号登录" />
         <WhiteSpace size="lg" />
         <WingBlank>
-          <form action="">
+          <form onSubmit={this.login}>
             <div className={styles.formItem}>
               <input
                 className={styles.input}
+                value={username}
+                name="username"
                 placeholder="请输入账号"
                 type="text"
+                onChange={this.changeValue}
               />
             </div>
             <div className={styles.formItem}>
               <input
                 className={styles.input}
+                value={password}
+                name="password"
                 placeholder="请输入密码"
                 type="text"
+                onChange={this.changeValue}
               />
             </div>
             <div className={styles.formSubmit}>
@@ -31,9 +71,9 @@ export default class Login extends Component {
             </div>
           </form>
           <Flex className={styles.backHome}>
-              <Flex.Item>
-                  <a href="#/">还没有账号，去注册~</a>
-              </Flex.Item>
+            <Flex.Item>
+              <a href="#/">还没有账号，去注册~</a>
+            </Flex.Item>
           </Flex>
         </WingBlank>
       </div>
